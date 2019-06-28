43b661bc9c0be009a583e40fbd0bec51cf711d93
Cindy <cindyxmiao@gmail.com>
2019-06-28 19:12:58 +0200

    changing grid size with user prompt

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
+numGrid = sizeOfGrid();
 var i = 1;
-const maxboxes = 100;
-
+const maxboxes = numGrid*numGrid;
 
     while (i <= maxboxes){
     const container = document.querySelector('#container');
@@ -14,13 +14,14 @@ const maxboxes = 100;
         i++;
     }
 
-changeGridSize();
 
-
-function changeGridSize(){
-    grid = prompt("please enter grid size", "10");
-   
+//calculates size of boxes;
+function sizeOfGrid(){
+    numGrid = prompt("please enter grid size", "10");
+    gridSize = (700 - 0.1*numGrid*2)/numGrid;
+   return numGrid;
 }
+
 
 //add event listener for hovering to a group of nodes
 
@@ -30,6 +31,8 @@ function changeGridSize(){
             var boxnum = event.currentTarget.id;
             paint (boxnum);
         });
+        box.style.width = gridSize + 'px';
+        box.style.height = gridSize + 'px';
     });
 
 //changes color of hovered box
@@ -38,5 +41,6 @@ function paint (boxnum){
     const drawOn = document.getElementById(boxnum);
     drawOn.id = "painted";
 }
+
 
 

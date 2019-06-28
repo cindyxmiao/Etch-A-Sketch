226b6ee7850b15ced4448e08d826fddcb14c151b
Cindy <cindyxmiao@gmail.com>
2019-06-28 20:38:33 +0200

    Added button to prompt grid change

--- myscript.js
+++ myscript.js
@@ -1,6 +1,20 @@
-numGrid = sizeOfGrid();
-var i = 1;
-const maxboxes = numGrid*numGrid;
+var squareDim = createGrid();
+
+var btn = document.querySelector ('#btn');
+btn.addEventListener ('click', () =>{
+    squareDim = createGrid();
+});
+
+
+
+//creates grid given number of grid squares
+function createGrid(){
+
+    clearGrid();
+    var numGrid = sizeOfGrid();
+    var squareDim = (650 - 0.02*numGrid*2)/numGrid;
+    const maxboxes = numGrid*numGrid;
+    var i = 1;
 
     while (i <= maxboxes){
     const container = document.querySelector('#container');
@@ -14,33 +28,44 @@ const maxboxes = numGrid*numGrid;
         i++;
     }
 
+    const boxes = document.querySelectorAll('.gridSquare');
+    boxes.forEach((box) => {
+    box.addEventListener ('mouseover',(e) =>{
+        var boxnum = event.currentTarget.id;
+        paint (boxnum);
+    });
+    box.style.width = squareDim + 'px';
+    box.style.height = squareDim + 'px';
+});
 
-//calculates size of boxes;
+    return squareDim;
+}
+
+//clears grid
+
+function clearGrid(){
+    var contain = document.getElementById('container');
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+
+//prompts user for grid size, returs size;
 function sizeOfGrid(){
-    numGrid = prompt("please enter grid size", "10");
-    gridSize = (700 - 0.1*numGrid*2)/numGrid;
+    do {
+        numGrid = prompt("please enter a grid size between 1 and 100");
+
+    } while (numGrid < 1 || numGrid >100);
    return numGrid;
 }
 
 
-//add event listener for hovering to a group of nodes
-
-    const boxes = document.querySelectorAll('.gridSquare');
-    boxes.forEach((box) => {
-        box.addEventListener ('mouseover',(e) =>{
-            var boxnum = event.currentTarget.id;
-            paint (boxnum);
-        });
-        box.style.width = gridSize + 'px';
-        box.style.height = gridSize + 'px';
-    });
-
 //changes color of hovered box
-
 function paint (boxnum){
     const drawOn = document.getElementById(boxnum);
     drawOn.id = "painted";
 }
+
 
 
 
